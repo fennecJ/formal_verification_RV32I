c9d24a14e99dfa3327050cff7e20d0bbf298ecf7
fennecJ <hwahwa649@gmail.com>
2024-10-03 16:09:29 +0800

    Constraint fetched instruction to be always valid

--- isa.tcl
+++ isa.tcl
@@ -51,11 +51,68 @@ assume {core.if_unit.parcel_exceptions == 0}
 assume {core.wb_exceptions == 0}
 assume {core.id_unit.my_exceptions == 0}
 
-# stopat core.if_unit.rv_instr
-# assume {core.if_unit.rv_instr == formaltest_RV12.rvfi_input_insn}
-# stopat core.if_unit.rv_instr
-# is_validInst is defined in isa.sva
-# inst_always_valid: assume {is_validInst(core.if_unit.rv_instr)}
+# Fetched instruction always valid RV32I instruction
+stopat core.if_unit.rv_instr
+assume {
+    core.if_unit.rv_instr[6:0] == 7'b0110111 ||     // LUI
+    core.if_unit.rv_instr[6:0] == 7'b0010111 ||     // AUIPC
+    core.if_unit.rv_instr[6:0] == 7'b1101111 ||     // JAL
+    core.if_unit.rv_instr[6:0] == 7'b1100111 ||     // JALR
+
+    (core.if_unit.rv_instr[6:0] == 7'b1100011       // B-type (Branch)
+     && core.if_unit.rv_instr[14:12] != 3'b010  
+     && core.if_unit.rv_instr[14:12] != 3'b011
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0000011       // L-type
+     && core.if_unit.rv_instr[14:12] != 3'b011
+     && core.if_unit.rv_instr[14:12] != 3'b110
+     && core.if_unit.rv_instr[14:12] != 3'b111
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0100011       // S-type
+     && core.if_unit.rv_instr[14:12] != 3'b000
+     && core.if_unit.rv_instr[14:12] != 3'b001
+     && core.if_unit.rv_instr[14:12] != 3'b010
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0010011       // I-type 1
+     && core.if_unit.rv_instr[14:12] != 3'b001
+     && core.if_unit.rv_instr[14:12] != 3'b101
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0010011       // I-type 2 (logit shift)
+     && core.if_unit.rv_instr[31:25] == 7'b000
+     && core.if_unit.rv_instr[14:12] == 3'b?01
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0010011       // I-type 3 (arith shift)
+     && core.if_unit.rv_instr[31:25] == 7'b0100000
+     && core.if_unit.rv_instr[14:12] == 3'b101
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0110011       // R-type 1
+     && core.if_unit.rv_instr[31:25] == 7'b000
+    ) ||
+
+    (core.if_unit.rv_instr[6:0] == 7'b0110011       // R-type 2
+     && core.if_unit.rv_instr[31:25] == 7'b0100000
+     && (core.if_unit.rv_instr[14:12] == 3'b101 
+         || core.if_unit.rv_instr[14:12] == 3'b000
+        )
+    ) ||
+    
+    (core.if_unit.rv_instr[6:0] == 7'b0001111       // FENCE, FENCE.TSO, PAUSE
+     && core.if_unit.rv_instr[14:12] == 3'b000
+    ) ||
+   
+    (core.if_unit.rv_instr[6:0] == 7'b1110011       // ECALL, EBREAK
+     && core.if_unit.rv_instr[19:7] == 13'b0
+     && (core.if_unit.rv_instr[31:20] == 12'd0 
+         || core.if_unit.rv_instr[31:20] == 12'd1
+        )
+    )
+};
 
 task -set <embedded>
 set_proofgrid_per_engine_max_local_jobs 15
